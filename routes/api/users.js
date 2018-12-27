0b634b1084c32f0c3a9a8bdfd9432c31c5d8ae76
Rikey Chen <rikey.chen16@gmail.com>
2018-12-27 14:44:21 -0800

    Update login route to use jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,7 +45,22 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then((isMatch) => {
           if (isMatch) {
-            res.json({ msg: 'Success' });
+            const payload = {
+              id: user.id,
+              handle: user.handle,
+              email: user.email,
+            };
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token,
+                });
+              },
+            );
           } else {
             return res.status(400).json({ password: 'Incorrect password' });
           }
