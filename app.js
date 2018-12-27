316a5a40b4fdbcd11b256b4617b759f1c81d5068
Rikey Chen <rikey.chen16@gmail.com>
2018-12-26 17:34:08 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('This is working'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
