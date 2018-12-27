f53f976366c6e9c99b331e5e46178df9472a052e
Rikey Chen <rikey.chen16@gmail.com>
2018-12-26 20:17:38 -0800

    Move bodyParser import above db import

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+const bodyParser = require('body-parser');
 
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
-const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
