af583cf9f3c30174367ec456276289c85e089f97
Rikey Chen <rikey.chen16@gmail.com>
2018-12-27 14:37:10 -0800

    Add login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
+const keys = require('../../config/keys');
 
 const router = express.Router();
 
@@ -27,6 +29,27 @@ router.post('/register', (req, res) => {
             .catch(err => console.log(err));
         });
       });
+    });
+});
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ email: 'This user does not exist' });
+      }
+
+      bcrypt.compare(password, user.password)
+        .then((isMatch) => {
+          if (isMatch) {
+            res.json({ msg: 'Success' });
+          } else {
+            return res.status(400).json({ password: 'Incorrect password' });
+          }
+        });
     });
 });
 
