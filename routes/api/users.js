7f6b0ea73302f64958c9f7601c649eb03a0a72c9
Rikey Chen <rikey.chen16@gmail.com>
2018-12-27 15:39:21 -0800

    Add login validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -41,6 +41,12 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
