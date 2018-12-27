3bfe8fb21ca5ed55392d8aa2c72eda0ec72d4e8a
Rikey Chen <rikey.chen16@gmail.com>
2018-12-26 18:02:40 -0800

    Add bodyParser

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -15,6 +16,8 @@ mongoose
 app.get('/', (req, res) => res.send('This is working'));
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
