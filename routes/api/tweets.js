a887560e0665ee493a30346e472c54a9e458b43d
Rikey Chen <rikey.chen16@gmail.com>
2018-12-26 17:56:21 -0800

    Add initial routes

---
+++ routes/api/tweets.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/test', (req, res) => res.json({ msg: 'This is the tweets route' }));
+
+module.exports = router;
