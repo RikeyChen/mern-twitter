7b4eb17f15cf3ee52a4f764912f42738be4a0774
Rikey Chen <rikey.chen16@gmail.com>
2018-12-27 16:25:00 -0800

    Update passport authentication route to return the user

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,7 +15,11 @@ router.get(
   '/current',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    res.json({ msg: 'Success' });
+    res.json({
+      id: req.user.id,
+      handle: req.user.handle,
+      email: req.user.email,
+    });
   },
 );
 
