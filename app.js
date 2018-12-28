ceadfb4289d13de400dcc133018533d4322fdbe3
Rikey Chen <rikey.chen16@gmail.com>
2018-12-27 18:49:20 -0800

    Fix bug with missing invocation of passport

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose
   .catch(err => console.log(err));
 
 app.use(passport.initialize());
-require('./config/passport');
+require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
