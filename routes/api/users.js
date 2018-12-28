222c911bdd5577e13ad71cfbb7bbe649ab5365be
Rikey Chen <rikey.chen16@gmail.com>
2018-12-27 16:19:37 -0800

    Add passport authentication route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 const User = require('../../models/User');
 const keys = require('../../config/keys');
 const validateRegisterInput = require('../../validation/register');
@@ -9,6 +10,14 @@ const validateLoginInput = require('../../validation/login');
 const router = express.Router();
 
 router.get('/test', (req, res) => res.json({ msg: 'This is the users route' }));
+
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({ msg: 'Success' });
+  },
+);
 
 router.post('/register', (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
