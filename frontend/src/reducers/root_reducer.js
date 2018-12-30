fec9155002c52f8faf89bed66ce28a40c679eade
Rikey Chen <rikey.chen16@gmail.com>
2018-12-30 14:42:44 -0800

    Add tweets reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import tweets from './tweets_reducer';
 import session from './session_reducer';
 import errors from './errors_reducer';
 
 const RootReducer = combineReducers({
+  tweets,
   session,
   errors,
 });
